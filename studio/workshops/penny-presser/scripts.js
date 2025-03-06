62b20ee52af8b2a4fb278210645103e201b40242
aprilhoff <hoffa617@newschool.edu>
2025-03-06 09:56:55 -0500

    .

--- studio/workshops/penny-presser/scripts.js
+++ studio/workshops/penny-presser/scripts.js
@@ -2,18 +2,21 @@
 var pressButton = document.querySelector('button[name="press"]');
 var pennySelect = document.querySelector('select[name="penny"]');
 var pennyImage = document.querySelector('.penny');
-
+var studentSelect = document.querySelector('select[name="student"]');
 
 // console log elements
 console.log(pressButton);
 console.log(pennySelect.value);
-
+// log student value 
+console.log(studentSelect.value);
 
 // add event listener to the press button 
 pressButton.addEventListener('click', ()=>{
     console.log(pennySelect.value);
     // set the penny Image src
-    pennyImage.src = 'img/' + pennySelect.value;
+    // pennyImage.src = 'img/' + pennySelect.value;
+
+    pennyImage.src = 'https://' + studentSelect.value + '.interactive.rodeo/studio/workshops/penny-presser/img/' + pennySelect.value
 
     // console log penny image 
     console.log(pennyImage);
